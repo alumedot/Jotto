1b5056f000043e36dda2098ec8f63d08ecf842be
Alexey Kozlov <alexey.kozlov@orange.com>
2020-02-22 19:14:13 +0300

    Start tests for GuessedWords component

--- src/GuessWords/GuessedWords.tsx
+++ src/GuessWords/GuessedWords.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { IProps } from './types';
 
 
-const GuessedWords = () => {
+const GuessedWords = (props: IProps) => {
+    let contents = null;
+    if (props.guessedWords.length === 0) {
+        contents = (
+            <span data-test="guess-instructions">
+                Try to guess the secret word!
+            </span>
+        );
+    }
+
     return (
-        <div>
-
+        <div data-test="component-guessed-word">
+            { contents }
         </div>
     )
 };
 
 GuessedWords.propTypes = {
-
+    guessedWords: PropTypes.arrayOf(
+        PropTypes.shape({
+            guessedWord: PropTypes.string.isRequired,
+            letterMatchCount: PropTypes.number.isRequired,
+        })
+    ).isRequired,
 };
 
 export default GuessedWords;
--- src/GuessWords/types.ts
+++ src/GuessWords/types.ts
@@ -1,3 +1,10 @@
-export interface IProps {
+export interface IGuessedWords {
+    guessedWord: string;
+    letterMatchCount: number;
+}
 
+export interface IProps {
+    // secretWord: string;
+    // success: boolean;
+    guessedWords: IGuessedWords[];
 }
