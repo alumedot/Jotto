0b7b4e1a250eb2136c4434ffad20eeec5e0d30a6
Alexey Kozlov <alumedot@icloud.com>
2020-02-22 20:57:45 +0300

    - Bootstrap installed, - styling to the components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
 
+// Components
+import GuessedWords from './GuessWords';
 import Congrats from './Congrats';
 
+import './App.css';
 
-const App = () => {
-  return (
-    <div className="App">
-      <Congrats success={true} />
-    </div>
-  );
-};
+
+class App extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h1>Jotto</h1>
+                <Congrats success={true} />
+                <GuessedWords guessedWords={[
+                    { guessedWord: 'train', letterMatchCount: 3 },
+                    { guessedWord: 'train', letterMatchCount: 3 },
+                    { guessedWord: 'train', letterMatchCount: 3 },
+                ]} />
+            </div>
+        );
+    }
+}
 
 export default App;
